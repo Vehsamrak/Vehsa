1e283ab686b14d421d40081e087a91cf30b61276
Petr Karmashev (Rottenwood) <smonkl@bk.ru>
2016-04-14 21:25:58 +0300

    Config loader introduced

---
+++ src/Config.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Vehsamrak\Vehsa;
+
+use Vehsamrak\Vehsa\Exception\ConfigParameterNotFound;
+
+/**
+ * Application configuration
+ * Usage: Vehsamrak\Vehsa\Config::get('database_type');
+ * @author Vehsamrak
+ */
+class Config
+{
+
+    public static function get($key)
+    {
+        $config = require(__DIR__ . '/../../config.php');
+
+        if (!isset($config[$key])) {
+        	throw new ConfigParameterNotFound;
+        }
+
+        return $config[$key] ;
+    }
+}
