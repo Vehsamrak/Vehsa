64b7b504636e09180540cfe8aac9a2e21ea44ef0
Petr Karmashev (Rottenwood) <smonkl@bk.ru>
2016-04-14 21:29:54 +0300

    Template view renderer introduced

---
+++ src/Renderer.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Vehsamrak\Vehsa;
+
+/**
+ * Template view renderer
+ * @author Vehsamrak
+ */
+class Renderer
+{
+
+    /**
+     * Render view template.
+     * Parameters are extracted for usage inside template.
+     * @param string $template
+     * @param array  $parameters
+     */
+    public function render($template, array $parameters = [])
+    {
+        if (is_array($template)) {
+            $parameters = $template;
+            $template = 'index';
+        }
+
+        $templateFileName = $template . '.php';
+
+        extract($parameters);
+
+        require_once(__DIR__ . '/../View/' . $templateFileName);
+    }
+}
