5680db358db079dfb7ea887e7bc883f678ab652f
Petr Karmashev (Rottenwood) <smonkl@bk.ru>
2016-04-17 12:11:48 +0300

    Renderer::renderException() method

--- src/Renderer.php
+++ src/Renderer.php
@@ -9,6 +9,8 @@ namespace Vehsamrak\Vehsa;
 class Renderer
 {
 
+    const VIEW_PATH = __DIR__ . DIRECTORY_SEPARATOR . 'View';
+
     /**
      * Render view template.
      * Parameters are extracted for usage inside template.
@@ -24,8 +26,60 @@ class Renderer
 
         $templateFileName = $template . '.php';
 
+        $templatePath = join(
+            DIRECTORY_SEPARATOR,
+            [
+                $this->getUserViewDirectory(),
+                $templateFileName,
+            ]
+        );
+
         extract($parameters);
 
-        require_once(Config::get('root_directory') . '/View/' . $templateFileName);
+        require_once($templatePath);
+    }
+
+    /**
+     * @param \Exception $exception
+     * @throws Exception\ConfigParameterNotFound
+     */
+    public function renderException(\Exception $exception)
+    {
+        $templatePath = join(
+            DIRECTORY_SEPARATOR,
+            [
+                $this->getUserViewDirectory(),
+                'Errors',
+                'exception.php',
+            ]
+        );
+
+        if (!file_exists($templatePath)) {
+            $templatePath = join(
+                DIRECTORY_SEPARATOR,
+                [
+                    self::VIEW_PATH,
+                    'exception.php',
+                ]
+            );
+        }
+
+        extract(['exception' => $exception]);
+
+        require_once($templatePath);
+    }
+
+    /**
+     * @return string
+     */
+    private function getUserViewDirectory()
+    {
+        return join(
+            DIRECTORY_SEPARATOR,
+            [
+                Config::getUserRootDirectory(),
+                'View',
+            ]
+        );
     }
 }
